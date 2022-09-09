824a8264a2d92fb0a41343ba71c1048f977218d9
Angela Guo <aguo921@aucklanduni.ac.nz>
2022-09-10 10:44:15 +1200

    Add breakpoints

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,20 @@ import Container from "@mui/material/Container";
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 
+import { useTheme } from '@mui/material/styles';
+
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 export default function Home() {
+
+  const theme = useTheme();
+  const small = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
     <Layout>
       <Header />
       <Container maxWidth="sm">
-        <Typography variant="h1" sx={{py: 20}}>Library</Typography>
+        <Typography variant={small ? 'h3' : 'h1'} sx={{py: 20}}>Library</Typography>
       </Container>
     </Layout>
   )
