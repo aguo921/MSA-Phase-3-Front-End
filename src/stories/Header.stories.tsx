f8d80d59f560f88ad2f52368239f53d8099c4ef8
Angela Guo <aguo921@aucklanduni.ac.nz>
2022-09-10 10:45:19 +1200

    Add viewpoints to test header stories

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -8,6 +8,9 @@ import Header from '../components/Header';
 export default {
   title: 'Header',
   component: Header,
+  parameters: {
+    chromatic: { viewports: [320, 600, 1200] }
+  },
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = () => <Header />;
@@ -16,8 +19,16 @@ export const Default = Template.bind({});
 
 export const Search = Template.bind({});
 Search.play = async () => {
-    await waitFor(() => userEvent.type(screen.getByLabelText('Search the library...'), 'Harry Potter'));
-    await waitFor(() => userEvent.click(screen.getByLabelText('Search by...')));
-    await waitFor(() => userEvent.click(screen.getByText(/Title/)));
-    await waitFor(() => userEvent.click(screen.getByLabelText('search')));
+    await waitFor(
+      async () => userEvent.type(screen.getByLabelText('Search the library...'), 'Harry Potter')
+    );
+    await waitFor(
+      async () => userEvent.click(screen.getByLabelText('Search by...'))
+    );
+    await waitFor(
+      async () => userEvent.click(screen.getByText(/Title/))
+    );
+    await waitFor(
+      async () => userEvent.click(screen.getByLabelText('search'))
+    );
 };
